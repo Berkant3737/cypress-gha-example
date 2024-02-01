e1b47377d3ff13f90043ee0bca22f0c78ca8172e
Berkant <berkant.hurma@tasarlab.com>
2024-02-01 17:37:51 +0300

    ..

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  e2e: {
-    baseUrl: "https://example.cypress.io",
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
-  },
+  baseUrl: "https://example.cypress.io",
+  // Diğer Cypress konfigürasyonları...
+  // setupNodeEvents gerekmiyorsa bu kısmı kaldırabilirsiniz.
 });
